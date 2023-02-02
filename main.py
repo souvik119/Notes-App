83640ffa11c4a9c6d4883e2e01e72570d3112188
souvikghosh <Sayali1@>
2023-02-01 16:59:22 -0800

    built out website

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-# reason we can do that is because of __init__.py website is a package
 from website import create_app
 
 app = create_app()
 
-# only run the webserver if main.py is run directly
 if __name__ == '__main__':
-    # everytime a change is made, re-run the server because debug is True
-    # should turn it off for prod env
     app.run(debug=True)
--- website/__init__.py
+++ website/__init__.py
@@ -1,16 +1,41 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
-    # in prod env do not share this key with anyone
-    app.config['SECRET_KEY'] = 'cbwhberyfekj33zs'
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
-    # Registering the views
     from .views import views
     from .auth import auth
 
-    # these will be the prefix before actual url for the server
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+    
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
